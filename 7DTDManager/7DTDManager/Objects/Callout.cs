2904fb27338e05540514426e1763ff25dd308184
c0nnex <me@c0nnex.de>
2015-04-11 04:54:36 +0200

    Introduced callouts

---
+++ 7DTDManager/7DTDManager/Objects/Callout.cs
@@ -0,0 +1,48 @@
+﻿using _7DTDManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DTDManager.Objects
+{
+    public enum CalloutType
+    {
+        Message,
+        Error
+    }
+
+    public class Callout
+    {
+        public IPlayer Target { get; set; }
+        public DateTime When { get; set; }
+        public CalloutType What { get; set; }
+        public String Message { get; set; }
+        public bool Done { get; set; }
+
+        public Callout()
+        {
+            Done = false;
+        }
+
+        public Callout(IPlayer target,TimeSpan when, CalloutType what, string msg) : this()
+        {
+            Target = target;
+            When = DateTime.Now + when;
+            What = what;
+            Message = msg;
+        }
+
+        public virtual void Execute()
+        {
+            Done = true;
+        }
+
+        public virtual void SetDone(bool done = true)
+        {
+            Done = done;
+        }
+    }
+
+}
---
+++ 7DTDManager/7DTDManager/Objects/CalloutManager.cs
@@ -0,0 +1,71 @@
+﻿using _7DTDManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DTDManager.Objects
+{
+    public class CalloutManager
+    {
+        static List<Callout> AllCallouts;
+        static DateTime nextCallout = DateTime.MaxValue;
+        static DateTime lastCallout = DateTime.Now;
+
+        public static DateTime NextCallout
+        {
+            get { return nextCallout; }
+        }
+
+        public static void RegisterCallout(Callout callout)
+        {
+            AllCallouts.Add(callout);
+            UpdateCallouts();
+        }
+
+        public static void UpdateCallouts()
+        {
+            DateTime now = DateTime.Now;
+
+            // First execute callouts
+            foreach (var item in AllCallouts)
+            {
+                if (item.When <= now )
+                {
+                    item.Execute();
+                }
+            }
+            Housekeeping();
+
+            nextCallout = DateTime.MaxValue;
+
+            foreach (var item in AllCallouts)
+            {
+                if (item.When < nextCallout)
+                    nextCallout = item.When;
+            }
+        }
+
+        public static void Housekeeping()
+        {
+            List<Callout> newList = new List<Callout>();
+            foreach (var item in AllCallouts)
+            {
+                if (!item.Done)
+                    newList.Add(item);
+            }
+            AllCallouts = newList;
+        }
+
+        public static void UnregisterCalloutsForPlayer(IPlayer p)
+        {
+            foreach (var item in AllCallouts)
+            {
+                if (item.Target == p)
+                    item.SetDone();
+            }
+            UpdateCallouts();
+        }
+    }
+}
