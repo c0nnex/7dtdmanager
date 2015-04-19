97e2bd4b8850c945f10165b88af106ba4d2e1679
c0nnex <me@c0nnex.de>
2015-04-19 06:30:02 +0200

    Afterpost

---
+++ 7DTDManager/7DTDManager/Commands/AdminCommands/cmdReload.cs
@@ -0,0 +1,39 @@
+﻿using _7DTDManager.Interfaces;
+using _7DTDManager.Interfaces.Commands;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DTDManager.Commands.AdminCommands
+{
+    public class cmdReload : AdminCommandBase
+    {        
+        public cmdReload()
+        {
+            CommandName = "reload";
+            CommandHelp = "Reload configuration";
+            CommandUsage = "/reload";
+        }
+
+        public override bool Execute(IServerConnection server, IPlayer p, params string[] args)
+        {
+            try
+            {
+                logger.Info("reloading config");
+                Config.Configuration reloadedConfig = Config.Configuration.Load();
+                Program.Config = reloadedConfig;               
+                p.Message("Configuration reloaded.");
+            }
+            catch ( Exception ex)
+            {
+                p.Error("Error reloading config: {0}", ex.Message);
+                logger.Error("Error reloading config");
+                logger.Error(ex.ToString());
+            }
+            return true;
+        }
+    }
+}
---
+++ 7DTDManager/7DTDManager/Commands/cmdMail.cs
@@ -0,0 +1,65 @@
+﻿using _7DTDManager.Interfaces;
+using _7DTDManager.Interfaces.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DTDManager.Commands
+{
+    public class cmdMail : PublicCommandBase
+    {
+        public cmdMail()
+        {
+            CommandHelp = "Read/write a mail";
+            CommandName = "mail";
+            CommandCost = 0;
+            CommandUsage = "/mail read or /mail [targetplayer] [message]";
+            CommandArgs = 0;
+        }
+
+        public override bool Execute(IServerConnection server, IPlayer p, params string[] args)
+        {
+            if (args.Length == 1)
+            {
+                if (p.Mail.Count > 0)
+                    p.Confirm("You have {0} unread mail(s). Use '/mail read' to read them.", p.Mail.Count);
+                else
+                    p.Confirm("You have no unread mail.");
+                return true;
+            }
+            if (args[1] == "read")
+            {
+                if (p.Mail.Count <= 0)
+                {
+                    p.Error("No unread mail.");
+                    return true;
+                }
+                MailMessage mail = p.Mail[0];
+                p.Mail.RemoveAt(0);
+                p.Message("From: {0}", server.AllPlayers.FindPlayerBySteamID(mail.FromSteamID).Name);
+                p.Message("Date: {0} {1}", mail.When.ToShortDateString(), mail.When.ToShortTimeString());
+                p.Message("Text: {0}", mail.Message);
+                return true;
+            }
+            IPlayer targetPlayer = server.AllPlayers.FindPlayerByName(args[1],false);
+            if ((targetPlayer == null))
+            {
+                p.Message("Targetplayer '{0}' was not found.", args[1]);
+                return false;
+            }
+            if (args.Length < 3)
+            {
+                p.Error(CommandUsage);
+                return true;
+            }
+            string restCmd = String.Join(" ", args, 2, args.Length - 2);
+            MailMessage newMail = new MailMessage { FromSteamID = p.SteamID, When = DateTime.Now, Message = restCmd };
+            targetPlayer.AddMail(newMail);
+            p.Confirm("Mail sent to '{0}'.", targetPlayer.Name);
+            return true;
+        }
+        
+    }
+}
