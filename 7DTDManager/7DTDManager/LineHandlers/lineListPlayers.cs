2c9c15276b4175c4d1a134bbe2cab18b5e07f1f2
c0nnex <me@c0nnex.de>
2015-05-05 01:01:11 +0200

    Fixed possible missed logouts

--- 7DTDManager/7DTDManager/LineHandlers/lineListPlayers.cs
+++ 7DTDManager/7DTDManager/LineHandlers/lineListPlayers.cs
@@ -13,11 +13,24 @@ namespace _7DTDManager.LineHandlers
     {
         static Logger logger = LogManager.GetCurrentClassLogger();
         static Regex rgLPLine = new Regex("^[0-9]+. id=(?<enityid>[0-9]+), (?<name>.*), pos=\\((?<pos>.*)\\), rot=.*, deaths=(?<deaths>[0-9]+), zombies=(?<zombies>[0-9]+), players=(?<players>[0-9]+), score=.*, steamid=(?<steamid>[0-9]+), ip=(?<ip>.*), ping=(?<ping>[0-9]+)");
+        static Regex rgLPEnd = new Regex("Total of (?<numplayers>[0-9]+) in the game");
+
+        private List<IPlayer> found = null;
+        bool IsFirst = true;
+        int countPlayers = 0;
 
         public override bool ProcessLine(IServerConnection serverConnection, string currentLine)
         {
             if (rgLPLine.IsMatch(currentLine))
             {
+                if (IsFirst)
+                {
+                    countPlayers = 0;
+                    found = new List<IPlayer>();
+                    IsFirst = false;
+                    PriorityProcess = true;
+                }
+                countPlayers++;
                 Match match = rgLPLine.Match(currentLine);
                 GroupCollection groups = match.Groups;
                 if (Convert.ToInt32(groups["zombies"].Value) == 0)
@@ -30,14 +43,41 @@ namespace _7DTDManager.LineHandlers
                 p.SetIPAddress(groups["ip"].Value);
                 p.UpdateStats(Convert.ToInt32(groups["deaths"].Value), Convert.ToInt32(groups["zombies"].Value), Convert.ToInt32(groups["players"].Value), Convert.ToInt32(groups["ping"].Value));
                 p.UpdatePosition(groups["pos"].Value);
-                
+                found.Add(p);
                 //logger.Info("LP line {0} {1}", p.Name,p.EntityID);
                 return true;
+            }
+            if (rgLPEnd.IsMatch(currentLine))
+            {
+                IsFirst = true;
+                Match match = rgLPEnd.Match(currentLine);
+                GroupCollection groups = match.Groups;
+
+                PriorityProcess = false;
+                IsFirst = true;
+                int targetNum = Int32.Parse(groups["numplayers"].Value);
+                if (countPlayers != targetNum)
+                {
+                    logger.Warn("Number mismatch in ListPlayers! {0} != {1}", targetNum, countPlayers);
+                }
+                CleanupPlayers(serverConnection);
+                logger.Info("ListPlayers Parsing done.");       
             }
             return false;
         }
 
-        
+        private void CleanupPlayers(IServerConnection server)
+        {
+            foreach (var checkPlayer in server.AllPlayers.Players)
+            {
+                IPlayer pOnline = (from p in found where p.SteamID == checkPlayer.SteamID select p).FirstOrDefault();
+                if ((pOnline == null) && (checkPlayer.IsOnline))
+                {
+                    logger.Warn("Logout for {0} missed. Fixing.", checkPlayer.Name);
+                    checkPlayer.Logout();
+                }
+            }
+        }
         
     }
 }
